c7ab74790890ef749bc5cd78bcab0d519d47163d
Kushagra <kushagra198@gmail.com>
2024-06-10 20:46:15 +0530

    Typo

--- routes/books.ts
+++ routes/books.ts
@@ -9,9 +9,9 @@ import {
 
 const app = new Hono();
 
+app.post("/books", createBook); // To create a new book
 app.get("/books", getBooks); // To get all books
 app.get("/books/:id", getBookById); // To get a book by ID
-app.post("/books", createBook); // To create a new book
 app.put("/books/:id", updateBook); // To update an existing book
 app.delete("/books/:id", deleteBook); // To delete a book
 
